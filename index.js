040960c9d023af06ed6e7418109f123e5196ee30
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-21 22:24:08 +1000

    update ux blink

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function genepoem(event) {
     "You are an expert on Brazilian poetry. Your task is to find a well-known Brazilian poem that contains the word ${userwordInput.value}, and provide a 6-line summary of that poem. Each new sentence that starts with a capital letter should be on a new line. Use <br /> to separate the lines. Be concise, poetic, and ensure each line is short. Do not include a title to the poem. Sign the poem with 'Marley AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating Brazilian poem about ${userwordInput.value} for you...`;
+
   console.log("genepoem");
 
   axios.get(apiUrl).then(displayPoem);
