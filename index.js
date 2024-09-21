b1eea66107865419dde90abe79e78e1900e609f0
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-21 21:04:58 +1000

    input apiKey and update context

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ function genepoem(event) {
 
   let userwordInput = document.querySelector("#user-word");
   let apiKey = "3t4aaf46b1o5b1534f3e944eb00eb10d";
-  let prompt = `make sure will pick one brazilian poem about ${userwordInput}`;
-  let context = "show me a good brazilian poem";
+  let prompt = `User instructions: Generate a Brazilian poem about ${userwordInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Marley AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("genepoem");
